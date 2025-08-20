31f75625d8462d9337ababf47ad0884940c5ba34
SuperPhantomUser <scottymart07@gmail.com>
2025-08-19 22:03:35 -0400

    Deleted broken version

--- js/plugins/boat_pass.js
+++
@@ -1,6 +0,0 @@
-Game_Map.prototype.isBoatPassable = function(x, y) {
-    return this.terrainTag(x, y) === 1;
-};
-Game_Map.prototype.isShipPassable = function(x, y) {
-    return this.terrainTag(x, y) === 1;
-};
